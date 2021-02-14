e39849ad058e2cd7bae95486d4d29291f6a0c960
Dragon <715077252@qq.com>
2021-02-14 12:21:17 +0800

    update

--- src/sqlist.c
+++ src/sqlist.c
@@ -2,18 +2,39 @@
  * @Description: 
  * @Author: 
  * @Date: 2021-02-13 21:32:27
- * @LastEditTime: 2021-02-13 21:59:34
+ * @LastEditTime: 2021-02-14 12:20:25
  */
 #include<stdio.h>
+#include<malloc.h>
 typedef struct{
-    int v[100];
+    int *v;
     int len;
+    int max;
 }sqlist;
 void init(sqlist *s){
+    s->v=(int*)malloc(sizeof(int)*100);
     s->len=0;
+    s->max=100;
+
 }
 void add(sqlist *s,int position, int value){
     int i;
+    if(s->max=s->len){
+        s->max*=2;
+        s->len++;
+        int *n=(int*)malloc(sizeof(int)*s->max);
+        for(i=0;i<position;i++){
+            n[i]=s->v[i];
+        }
+        n[i]=value;
+        i++;
+        for(;i<s->len;i++){
+            n[i]=s->v[i];
+        }
+        free(s->v);
+        s->v=n;
+        return;
+    }
     s->len++;
     for(i=s->len-1;i>position;i--){
         s->v[i]=s->v[i-1];
@@ -27,6 +48,15 @@ void delete(sqlist *s, int position){
         s->v[i]=s->v[i+1];
     }
     s->len--;
+    if(s->len<s->max/4&&s->max!=0){
+        s->max/=2;
+        int *n=(int *)malloc(sizeof(int)*s->max);
+        for(i=0;i<s->len;i++){
+            n[i]=s->v[i];
+        }
+        free(s->v);
+        s->v=n;
+    }
     return;
 }
 int main(){
