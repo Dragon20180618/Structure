9781ce5c150bf493d087f335f082640719ad27f3
Dragon <715077252@qq.com>
2021-02-14 17:06:00 +0800

    link

---
+++ src/linklist.c
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Author: 
+ * @Date: 2021-02-14 12:50:54
+ * @LastEditTime: 2021-02-14 14:32:10
+ */
+#include<stdio.h>
+#include<malloc.h>
+typedef struct{
+    int v;
+    struct Linklist *next;
+}Linklist;
+int main(void){
+    Linklist s
+    return 0;
+}
--- src/tempCodeRunnerFile.c
+++
@@ -1,3 +0,0 @@
-
-    // printf("%d\t%d\t%d\n",s.v[1],s.len,s.max);
-    // printf("%d\t%d\t%d\n",s.v[2],s.len,s.max);
